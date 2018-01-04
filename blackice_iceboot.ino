c7f7bea1b28be86d7315b9c5dedd10eb26065ff3
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2018-01-04 14:21:46 -0800

    blackice fpga configure example now echos Serial from the fpga on PMOD[2] pin 88

--- blackice_iceboot.ino
+++ blackice_iceboot.ino
@@ -7,22 +7,32 @@
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  while (!Serial) ; // wait for USB serial connection to be established
+  Serial.println("setup() start");
+  Serial1.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, 1);
   myStorm.FPGAConfigure((const byte*)0x0801F000, 135100);
   digitalWrite(LED_BUILTIN, 0);
+  Serial.println("setup() done");
 }
 
 void loop()
 {
-  if (!Serial.available())
-    return;
-  digitalWrite(LED_BUILTIN, 1);
-  if (myStorm.FPGAConfigure(Serial)) {
-    while (Serial.available())
-      Serial.read();
+  if (Serial.available()) {
+    digitalWrite(LED_BUILTIN, 1);
+    if (myStorm.FPGAConfigure(Serial)) {
+      while (Serial.available())
+        Serial.read();
+      Serial.println("FPGAConfigure done");
+    }
+    digitalWrite(LED_BUILTIN, 0);
   }
-  digitalWrite(LED_BUILTIN, 0);
+  else if (Serial1.available() > 0) {
+    Serial.write(Serial1.read());
+  }
+  // else
+  //   return;
 }
 
