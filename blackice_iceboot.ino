769fe547dc34cf3524f2bbfe97414497fe00ab3c
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2018-01-07 18:03:24 -0800

    set scrollbar width and color

--- blackice_iceboot.ino
+++ blackice_iceboot.ino
@@ -2,44 +2,49 @@
 #include <SPI.h>
 #include <GD2.h>
 
-#define BLACK            0x000000
-#define VALHALLA         0x222034
-#define LOULOU           0x45283c
-#define OILED_CEDAR      0x663931
-#define ROPE             0x8f563b
-#define TAHITI_GOLD      0xdf7126
-#define TWINE            0xd9a066
-#define PANCHO           0xeec39a
+// DB32 Pallete
+// https://github.com/geoffb/dawnbringer-palettes
 
-#define GOLDEN_FIZZ      0xfbf236
-#define ATLANTIS         0x99e550
-#define CHRISTI          0x6abe30
-#define ELF_GREEN        0x37946e
-#define DELL             0x4b692f
-#define VERDIGRIS        0x524b24
-#define OPAL             0x323c39
+#define BLACK            0x000000  // #000000
+#define VALHALLA         0x222034  // #222034
+#define LOULOU           0x45283c  // #45283c
+#define OILED_CEDAR      0x663931  // #663931
+#define ROPE             0x8f563b  // #8f563b
+#define TAHITI_GOLD      0xdf7126  // #df7126
+#define TWINE            0xd9a066  // #d9a066
+#define PANCHO           0xeec39a  // #eec39a
 
-#define DEEP_KOAMARU     0x3f3f74
-#define VENICE_BLUE      0x306082
-#define ROYAL_BLUE       0x5b6ee1
-#define CORNFLOWER       0x639bff
-#define VIKING           0x5fcde4
-#define LIGHT_STEEL_BLUE 0xcbdbfc
-#define WHITE            0xffffff
-#define HEATHER          0x9badb7
+#define GOLDEN_FIZZ      0xfbf236  // #fbf236
+#define ATLANTIS         0x99e550  // #99e550
+#define CHRISTI          0x6abe30  // #6abe30
+#define ELF_GREEN        0x37946e  // #37946e
+#define DELL             0x4b692f  // #4b692f
+#define VERDIGRIS        0x524b24  // #524b24
+#define OPAL             0x323c39  // #323c39
+#define DEEP_KOAMARU     0x3f3f74  // #3f3f74
 
-#define TOPAZ            0x847e87
-#define DIM_GRAY         0x696a6a
-#define SMOKEY_ASH       0x595652
-#define CLAIRVOYANT      0x76428a
-#define BROWN            0xac3232
-#define MANDY            0xd95763
-#define PLUM             0xd77bba
-#define RAINFOREST       0x8f974a
-#define STINGER          0x8a6f30
+#define VENICE_BLUE      0x306082  // #306082
+#define ROYAL_BLUE       0x5b6ee1  // #5b6ee1
+#define CORNFLOWER       0x639bff  // #639bff
+#define VIKING           0x5fcde4  // #5fcde4
+#define LIGHT_STEEL_BLUE 0xcbdbfc  // #cbdbfc
+#define WHITE            0xffffff  // #ffffff
+#define HEATHER          0x9badb7  // #9badb7
+#define TOPAZ            0x847e87  // #847e87
+
+#define DIM_GRAY         0x696a6a  // #696a6a
+#define SMOKEY_ASH       0x595652  // #595652
+#define CLAIRVOYANT      0x76428a  // #76428a
+#define BROWN            0xac3232  // #ac3232
+#define MANDY            0xd95763  // #d95763
+#define PLUM             0xd77bba  // #d77bba
+#define RAINFOREST       0x8f974a  // #8f974a
+#define STINGER          0x8a6f30  // #8a6f30
 
 #define LINE_PIXEL_HEIGHT 8
 #define CHARACTERS_PER_LINE 60
+#define SCROLLBAR_WIDTH 20
+#define SCROLLBAR_HALF_WIDTH 10
 
 #define TAG_SCROLLBAR 201
 
@@ -155,9 +160,13 @@ void History::draw() {
     current_line_address = (current_line_address + (scrollback_length-1)) % scrollback_length;
   }
 
+  GD.ColorA(64); // 64/256
+  GD.cmd_bgcolor(VALHALLA);
+  GD.cmd_fgcolor(LIGHT_STEEL_BLUE);
+
   GD.Tag(TAG_SCROLLBAR);
-  GD.cmd_scrollbar(GD.w - 10, 5, 10, GD.h-10, OPT_FLAT, scrollbar_position - scrollbar_size_half, scrollbar_size, 65535);
-      GD.cmd_track(GD.w - 10, 5, 10, GD.h-10, TAG_SCROLLBAR);
+  GD.cmd_scrollbar(GD.w - SCROLLBAR_WIDTH, SCROLLBAR_HALF_WIDTH, SCROLLBAR_WIDTH, GD.h-SCROLLBAR_WIDTH, OPT_FLAT, scrollbar_position - scrollbar_size_half, scrollbar_size, 65535);
+      GD.cmd_track(GD.w - SCROLLBAR_WIDTH, SCROLLBAR_HALF_WIDTH, SCROLLBAR_WIDTH, GD.h-SCROLLBAR_WIDTH, TAG_SCROLLBAR);
 }
 
 
@@ -190,7 +199,7 @@ void loop() {
   char newchar;
   // for(i=0; i<GD.random(10); i++) {
   if (lines.line_count < 100) {
-    for(i=0; i<10; i++) {
+    for(i=0; i<40; i++) {
       newchar = get_char();
       lines.append_character(newchar);
       sprintf(linebuffer, "%-3u", lines.line_count);
